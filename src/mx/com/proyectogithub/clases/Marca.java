fbbd054b8f8398b7406a6fdfd5108ee353bbaabb
christian.solares <chrismsol@gmail.com>
2022-06-18 22:00:18 -0500

    Se agrega clase Marca

---
+++ src/mx/com/proyectogithub/clases/Marca.java
@@ -0,0 +1,20 @@
+package mx.com.proyectogithub.clases;
+
+public class Marca {
+	
+	private String nombreMarca;
+	private String objeto;
+	public String getNombreMarca() {
+		return nombreMarca;
+	}
+	public void setNombreMarca(String nombreMarca) {
+		this.nombreMarca = nombreMarca;
+	}
+	public String getObjeto() {
+		return objeto;
+	}
+	public void setObjeto(String objeto) {
+		this.objeto = objeto;
+	}
+	
+}
--- src/mx/com/proyectogithub/principal/ClasePrincipal.java
+++ src/mx/com/proyectogithub/principal/ClasePrincipal.java
@@ -1,6 +1,7 @@
 package mx.com.proyectogithub.principal;
 
 import mx.com.proyectogithub.clases.Figura;
+import mx.com.proyectogithub.clases.Marca;
 import mx.com.proyectogithub.clases.Persona;
 
 public class ClasePrincipal {
@@ -13,6 +14,7 @@ public class ClasePrincipal {
 		
 		Figura figura = new Figura();
 		Persona persona = new Persona();
+		Marca marca = new Marca();
 		
 		figura.setLados(4);
 		figura.setNombre("Cuadrado");
@@ -23,6 +25,11 @@ public class ClasePrincipal {
 		persona.setNombre("Chris");
 		
 		System.out.println("La persona "+persona.getNombre()+" tiene "+persona.getEdad()+" de edad.");
+		
+		marca.setNombreMarca("Nissan");
+		marca.setObjeto("Autos");
+		
+		System.out.println("La persona "+marca.getNombreMarca()+" fabrica "+marca.getObjeto()+".");
 		
 	}
 
