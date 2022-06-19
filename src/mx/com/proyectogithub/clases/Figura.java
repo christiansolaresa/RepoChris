6d294b50c1884ef0da9cf92f7d557703f476a59a
christian.solares <chrismsol@gmail.com>
2022-06-18 21:20:27 -0500

    Version inicial de proyecto1

---
+++ src/mx/com/proyectogithub/clases/Figura.java
@@ -0,0 +1,21 @@
+package mx.com.proyectogithub.clases;
+
+public class Figura {
+	
+	private int lados;
+	private String nombre;
+	
+	public int getLados() {
+		return lados;
+	}
+	public void setLados(int lados) {
+		this.lados = lados;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ src/mx/com/proyectogithub/principal/ClasePrincipal.java
@@ -0,0 +1,28 @@
+package mx.com.proyectogithub.principal;
+
+import mx.com.proyectogithub.clases.Figura;
+import mx.com.proyectogithub.clases.Persona;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Inicia proyecto Git.");
+		System.out.println("Modificación agregada desde Git Hub.");
+		
+		Figura figura = new Figura();
+		Persona persona = new Persona();
+		
+		figura.setLados(4);
+		figura.setNombre("Cuadrado");
+		
+		System.out.println("La figura con "+figura.getLados()+" es un "+figura.getNombre()+".");
+		
+		persona.setEdad(42);
+		persona.setNombre("Chris");
+		
+		System.out.println("La persona "+persona.getNombre()+" tiene "+persona.getEdad()+" de edad.");
+		
+	}
+
+}
